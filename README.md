9c2d2c88068bd65ab04543e5d3c2c45f9e63783b
DIR\felipe.tofoli <felipe.tofoli@concrete.com.br>
2019-02-12 15:33:04 -0200

    Updating README.md. Pre-processing image for better results.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # ocr-pytesseract
-POC's using pytesseract
+
+**POC using pytesseract, a wrapper of tesseract to python.**
+
+Below, some information to run the script.
+
+---
+
+####To run, execute:
+```
+$ python reader.py ["file-name-argument"]
+```
+---
+
+####Examples, with the image files of this repository:
+```
+$ python reader.py "images\phrase.jpg"
+```
+OR
+```
+$ python reader.py "images\clean-code-pt-br.jpeg"
+```
--- reader.py
+++ reader.py
@@ -1,6 +1,9 @@
 
 import sys
 import pytesseract as ocr
+import numpy as np
+import cv2 as cv
+
 from PIL import Image
 
 
@@ -9,7 +12,29 @@ ocr.pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\Tesseract-OCR\\tessera
 
 
 def getText(imageFileName):
-    return ocr.image_to_string(Image.open(imageFileName), lang='por')
+    # channel RGB
+    image = Image.open(imageFileName).convert('RGB')
+
+    # converting to an editable numpy array [x, y, channels]
+    npImage = np.asarray(image).astype(np.uint8)  
+
+    # decrease noisy before binarization
+    npImage[:, :, 0] = 0 # eliminating channel R (red)
+    npImage[:, :, 2] = 0 # eliminating channel B (blue)
+
+    # sets gray scale
+    grayImage = cv.cvtColor(npImage, cv.COLOR_RGB2GRAY) 
+
+    # Binary truncate for intensity
+    # Pixels with color intensity below 127 will be converted to 0 (black)
+    # Pixels with color intensity above 127 will be converted to 1 (white)    
+    # THRESH_OTSU uses intelligent analysisof truncate levels
+    ret, thresh = cv.threshold(grayImage, 127, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
+
+    # Recreates the image
+    preProcessedImage = Image.fromarray(thresh) 
+
+    return ocr.image_to_string(preProcessedImage, lang='por')
 
 
 def main (argv):
