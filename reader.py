47c03f1bf94e2b1ee293c7967f9832fcdbb910e9
DIR\felipe.tofoli <felipe.tofoli@concrete.com.br>
2019-02-12 15:00:27 -0200

    filename of the image to be analyzed by the OCR processor can ben passed by argv.

--- reader.py
+++ reader.py
@@ -1,4 +1,5 @@
 
+import sys
 import pytesseract as ocr
 from PIL import Image
 
@@ -7,5 +8,13 @@ from PIL import Image
 ocr.pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
 
 
-phrase = ocr.image_to_string(Image.open('images\\phrase.jpg'), lang='por')
-print(phrase)
+def getText(imageFileName):
+    return ocr.image_to_string(Image.open(imageFileName), lang='por')
+
+
+def main (argv):
+    imageFileName = argv[1]    
+    print(getText(imageFileName))
+
+if __name__ == "__main__":
+    main(sys.argv)
